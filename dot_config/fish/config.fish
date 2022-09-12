9172f900d3442dc6fdbb4b8d20dd28a6412ac484
Joakim L. Engeset <joakim.engeset@gmail.com>
2022-09-12 12:36:07 +0200

    Update dot_config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -51,4 +51,5 @@ if not set -q TMUX
   tmux new -A -s main
 end
 
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 starship init fish | source
