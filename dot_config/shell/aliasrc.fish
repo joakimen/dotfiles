f83646d0d136bfd9f8021a02f9006181f8f38754
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-09-22 22:56:57 +0200

    Update dot_config/shell/aliasrc Update dot_config/shell/aliasrc.fish

--- dot_config/shell/aliasrc.fish
+++ dot_config/shell/aliasrc.fish
@@ -36,38 +36,16 @@ alias fd "fd --hidden"
 alias kl kill-process
 alias standup "fin -c 10:45 | nvim"
 
-# tmux
-alias t "tmux new -A -s main"
-alias tmux "tmux -2 -u"
-alias tks "tmux kill-server"
-alias tn tmux-new-session
-
 # git
 alias g git
 #alias c "git add . && git commit"
 alias c "gi commit"
 alias cdd "cd $HOME/dev/github.com"
 alias cm chezmoi
-alias gw "gh repo view -w"
-alias fb switch-branch
-alias co switch-remote-branch
 alias ztig "bf git"
 alias lg lazygit
 alias nb "gi branch-create"
 alias cpr "gi pr-checkout"
-
-# docker
-#alias docker podman
-#alias d podman
-alias din docker-inspect
-alias dpsa "docker ps -a"
-alias dr docker-run
-alias dcu "docker-compose up"
-alias dco "docker-compose down"
-alias dps docker-image-push
-alias dil docker-image-list
-alias dcs docker-container-start
-alias dct docker-container-stop
 
 # maven
 alias mci "mvn clean install"
@@ -80,19 +58,10 @@ alias tfi "terraform init"
 alias tfp "terraform plan"
 alias tfa "terraform apply"
 
-# ansible
-alias ap ansible-playbook
-alias av ansible-vault
-alias update-macos "ansible-playbook $HOME/.config/ansible/mac.yml"
-
-
-
 # npm
 alias n npm
 alias ep "$EDITOR package.json"
 alias nr node-run
-alias sn "npm run snapshots"
-alias al "npm run build && npm run lint:fix && npm run snapshots && npm test -- -u"
 
 # gpg
 alias keys show-gpg-pubkey
@@ -101,10 +70,6 @@ if which eza &>/dev/null
     alias l 'eza -al'
     alias ls eza
     alias tree 'eza --tree'
-else if which lsd &>/dev/null
-    alias l 'lsd -l --date=relative'
-    alias ls lsd
-    alias tree 'lsd --tree'
 else
     alias l 'ls -hlGALF'
     alias ls 'ls -GAF'
