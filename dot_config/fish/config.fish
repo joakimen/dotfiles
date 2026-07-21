d4cbb3ee06d8ed3f111712a81354e9579f5d68e7
Joakim L. Engeset <joakim.engeset@gmail.com>
2026-07-21 12:31:44 +0200

    Update .Brewfile Update .bashrc Update .config/fish/config.fish Update .config/fish/functions/zd.fish Update .config/ghostty/config Update .config/lazygit/config.yml Update .config/nvim/init.lua Update .config/rg/config Update .config/starship.toml Update .config/zed/settings.json Update .config/zsh/.zshrc Update .default-go-packages

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,194 +1,165 @@
 # config.fish
 # Author: Joakim Engeset <joakim.engeset@gmail.com>
 
-alias ei "nvim ~/.config/nvim/init.lua"
-
-# set -xg EDITOR hx
+# --- Environment variables ---
 set -xg EDITOR nvim
 set -xg RIPGREP_CONFIG_PATH ~/.config/rg/config
-# set -xg FZF_DEFAULT_COMMAND 'fd --type f --hidden'
-# set -xg FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -xg FZF_DEFAULT_OPTS '--style full --height 50% --border --walker-skip .git,node_modules,.clj-kondo,.cpcache,.venv,lib'
 set -xg MANPAGER 'nvim +Man!'
 set -xg LC_ALL "en_US.utf-8"
 set -xg BAT_THEME base16
 set -xg XDG_CONFIG_HOME "$HOME/.config"
-
-#set -x DOCKER_HOST 'unix:///Users/joakle/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
-set -xg BUILDAH_FORMAT docker
 set -xg AWS_VAULT_PROMPT osascript
-# set -xg DEFAULT_REGION "eu-west-1"
-# set -xg AWS_DEFAULT_REGION "eu-west-1"
-# set -xg ANSIBLE_LOCALHOST_WARNING false
-# set -xg ANSIBLE_INVENTORY_UNPARSED_WARNING false
 set -xg GG_CLONE_DIR ~/dev/github.com
 set -xg CLONE_DIR ~/dev/github.com
 set -xg GG_GITHUB_USER joakimen
-set -xg SPACESHIP_EXIT_CODE_SHOW true
 set -xg ZELLIJ_DEFAULT_SESSION default-session
 set -xg HOMEBREW_NO_ENV_HINTS 1
 set -xg MR_CONFIG "$HOME/.mrconfig"
 set -xg MISE_NODE_DEFAULT_PACKAGES_FILE "$HOME/.config/mise/node-packages"
-# if updated, run $ mise where python@3.12 for the current path
-# set -xg CLOUDSDK_PYTHON "~/.local/share/mise/installs/python/3.12.3/bin/python3"
+set -xg LIFLIG_DEV "$HOME/dev/github.com/capralifecycle"
+set -xg ENABLE_LSP_TOOL 1
 
+# --- Settings ---
 set -g fish_greeting
 
-# regular keyboard
-bind \co "project-cd; commandline -f execute"
-# glove80
-bind alt-o "project-cd; commandline -f execute"
+# --- PATH ---
+fish_add_path ~/go/bin ~/bin /usr/local/sbin ~/.emacs.d/bin ~/.local/bin /opt/homebrew/bin ~/.babashka/bbin/bin ~/.bun/bin
 
-# bind \cb "gi branch-switch; commandline -f execute"
-
-# add support for multiple pattterns/teams
-bind f3 "kf-edit; commandline -f execute"
-bind f4 "awsvault exec; commandline -f execute"
-bind f5 "awsvault login; and commandline -f execute"
-
-set aliasfile $XDG_CONFIG_HOME/shell/aliasrc.fish
-
-set -xg LIFLIG_DEV "$HOME/dev/github.com/capralifecycle"
-
-alias acc="$LIFLIG_DEV/resources-definition/scripts/acc.ts"
-alias ac="e $aliasfile"
-
-function _source
-    [ -s $argv ] && . $argv
+# Set gcloud PATH directly (avoids cd which triggers mise hooks)
+set -l gcloud_bin "/opt/homebrew/share/google-cloud-sdk/bin"
+if test -d $gcloud_bin; and not contains $gcloud_bin $PATH
+    set -gx PATH $gcloud_bin $PATH
 end
 
+
+# --- Key bindings ---
+function project-cd
+    set -l project (scriv ls --absolute-paths | fzf)
+    and builtin cd $project
+end
+
+function fish_user_key_bindings
+    bind \co "project-cd; commandline -f execute"
+    bind alt-o "project-cd; commandline -f execute"
+    bind f3 "kf-edit; commandline -f execute"
+    bind f4 "awsvault exec; commandline -f execute"
+    bind f5 "awsvault login; and commandline -f execute"
+end
+
+# --- Abbreviations ---
+abbr a aws
+abbr g git
+abbr k kubectl
+abbr m make
+abbr n npm
+abbr cl claude
+abbr cm chezmoi
+abbr lg lazygit
+abbr tf terraform
+
+# --- Aliases ---
+alias lk "lefthook"
+alias e $EDITOR
+alias er "$EDITOR README.md"
+
+## k8s
+alias kt kubetail
+alias kx kubectx
+alias kn kubens
+alias kz kustomize
+
+# aws
+alias list-functions "aws lambda list-functions | jq '.Functions[].FunctionName' -r | sort"
+
+# clojure 
+alias bbi "bbin install"
+alias bbl "bbin ls"
+alias repl='clojure -J--enable-preview -Sdeps "{:deps {nrepl/nrepl {:mvn/version \"RELEASE\"} cider/cider-nrepl {:mvn/version \"RELEASE\"}}}" -M -m nrepl.cmdline --middleware "[\"cider.nrepl/cider-middleware\"]"'
+
+## system
+alias pid 'ps aux | rg'
+alias cdm "cd (mktemp -d)"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+alias ...... "cd ../../../../.."
+alias fd "fd --hidden"
+alias kl kill-process
+alias standup "fin -c 10:45 | nvim"
+alias uuid "uuidgen | tr '[:upper:]' '[:lower:]'"
+
+# git
+alias cdd "cd $HOME/dev/github.com"
+
+# maven
+alias mci "mvn clean install"
+alias mcv "mvn clean verify"
+alias mcsv "mvn clean spotless:apply verify"
+
+# npm
+alias ep "$EDITOR package.json"
+alias nr node-run
+
+# sd (SeeD)
+alias sdn "sd task new"
+alias sdl "sd task list"
+alias sds "sd task set"
+
+alias ev "nvim ~/.config/nvim/init.lua"
+alias acc "$LIFLIG_DEV/resources-definition/scripts/acc.ts"
+alias idea "$HOME/bin/idea"
+
+if command -q eza
+    alias l 'eza --all --long --header --group-directories-first --icons --git'
+    alias ls eza
+    alias tree 'eza --tree'
+else
+    alias l 'ls -hlGALF'
+    alias ls 'ls -GAF'
+end
+
+# --- Source external configs ---
 _source ~/.tokens.fish
 _source ~/.z.fish
 _source ~/.config/liflig/liflig.fish
-_source $aliasfile
 
-function fe
-    set file (fzf)
-    if test -n "$file"
-        $EDITOR $file
-    end
-end
+# --- Tool initialization ---
+_cached_source starship init fish --print-full-init
+_cached_source atuin init fish
+_cached_source fnox activate fish
 
-function kf-edit
-    set file (kf list | fzf)
-    if test -n "$file"
-        $EDITOR (eval echo $file)
-    end
-end
-
-# this file contains aliases to run cli tools (gh, etc)
-# with credentials from 1Password
-# _source ~/.config/op/plugins.sh
-
-fish_add_path ~/go/bin ~/bin /usr/local/sbin ~/.emacs.d/bin ~/.local/bin /opt/homebrew/bin ~/.babashka/bbin/bin
-# ~/.local/bin/google-cloud-sdk/bin
-
-set bindir "$HOME/bin"
-alias idea "$bindir/shell/idea"
-
-function sudo --description '!!-support for sudo'
-    if test "$argv" = !!
-        eval command sudo $history[1]
-    else
-        command sudo $argv
-    end
-end
-
-# Use fd instead of the default find for listing path candidates.
-function _fzf_compgen_path
-    fd --hidden --follow --exclude ".git" . "$1"
-end
-
-# Use fd to generate the list for directory completion
-function _fzf_compgen_dir
-    fd --type d --hidden --follow --exclude ".git" . "$1"
-end
-
-function aws_complete
-    complete --command $argv --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-end
-
-function timestamp --description "Simple timestamp for filenames"
-    date +'%Y-%m-%d_%H-%M-%S'
-end
-
-function ztig
-    set file (git ls-files | fzf --preview 'bat {} --line-range :30')
-    if test -n "$file"
-        lazygit -f "$file"
-    end
-end
-
-function erg
-    set matches (rg --vimgrep $argv[1])
-    if test $status -eq 0
-        printf "%s\n" $matches | nvim -c 'cb | copen' -
-    end
-end
-
-function nr
-    set scripts ($bindir/node/npmscripts.ts)
-    if test -z "$scripts"
-        return
-    end
-    set script (printf "%s\n" $scripts | gum filter --height 8)
-    if test -z "$script"
-        return
-    end
-    npm run "$script"
-end
-
-aws_complete aws
-aws_complete awslocal
-
-function bmise
-    switch $argv[1]
-        case install
-            set lang (mise plugin ls --core --user | fzf)
-            if test -z "$lang"
-                return
-            end
-            set lang_version (mise ls-remote "$lang" | fzf)
-            if test -z "$version"
-                return
-            end
-            echo "Installing $lang@$lang_version"
-            mise install "$lang@$lang_version"
-        case use
-            set lang (mise ls --json | jq 'keys[]' -r | fzf)
-            if test -z "$lang"
-                return
-            end
-            set lang_version (mise ls "$lang" --json | jq '.[].version' -r | fzf)
-            if test -z "$lang_version"
-                return
-            end
-            echo "Using $lang@$lang_version"
-            mise use "$lang@$lang_version"
-        case '*'
-            echo "Usage: bmise install|use <version>"
-    end
-end
-
-function kill-process
-    set pid (ps axo user=,pid=,time=,args= | fzf | awk '{print $2}')
-    if test -n "$pid"
-        echo "killing process: $pid"
-        kill -9 $pid
-    end
-end
-
-# The next line updates PATH for the Google Cloud SDK.
-source "/opt/homebrew/share/google-cloud-sdk/path.fish.inc"
-
-starship init fish | source
-atuin init fish | source
-
-# used to activate shims for non-interactive shells, such as when running VS Code
 if status is-interactive
-    mise activate fish | source
+    _cached_source mise activate fish
 else
     mise activate fish --shims | source
 end
 
-alias assume="source (brew --prefix)/bin/assume.fish"
+set -l safe_chain "$HOME/.safe-chain/scripts/init-fish.fish"
+test -f $safe_chain && source $safe_chain
+
+# ~/.config/fish/conf.d/sd.fish
+# functions -c fish_prompt _starship_prompt
+# function fish_prompt
+#   _starship_prompt
+#   if test -f ~/.local/share/sd/prompt.txt
+#     set_color bryellow
+#     printf ' %s' (cat ~/.local/share/sd/prompt.txt)
+#     set_color normal
+#   end
+# end
+
+function __sd_prompt_hook --on-event fish_prompt
+  if test -f ~/.local/share/sd/prompt.txt
+    set -gx SD_TASK (cat ~/.local/share/sd/prompt.txt)
+  else
+    set -e SD_TASK
+  end
+end
+
+function acc-loop
+    while read -P 'acc id: ' id; and test -n "$id"
+        acc get $id
+    end
+end
--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,8 +6,6 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.completeopt = 'menuone,noselect'
-vim.opt.termguicolors = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.tabstop = 2
@@ -16,9 +14,12 @@ vim.opt.expandtab = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.mouse = 'a'
-vim.wo.number = true
-vim.wo.signcolumn = 'yes'
+vim.opt.number = true
+vim.opt.signcolumn = 'yes'
+vim.opt.inccommand = 'split'
+vim.opt.smoothscroll = true
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -39,32 +40,62 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
     },
   },
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
   {
-    "gpanders/nvim-parinfer",
+    'gpanders/nvim-parinfer',
     ft = 'clojure',
     config = function()
-      parinfer_force_balance = true
-    end
+      vim.g.parinfer_force_balance = true
+    end,
   },
   {
-    'hrsh7th/nvim-cmp',
+    'saghen/blink.cmp',
+    version = '1.*',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
+      'fang2hou/blink-copilot',
+    },
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<Tab>'] = { 'snippet_forward', 'select_and_accept', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<CR>'] = { 'fallback' },
+      },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        documentation = { auto_show = true },
+      },
+      signature = { enabled = true },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = false,
+      },
     },
   },
 
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    lazy = false,
-  },
 
   {
     'catppuccin/nvim',
@@ -75,17 +106,65 @@ require('lazy').setup({
     end,
   },
 
-  'github/copilot.vim',
-
   {
     'nvim-telescope/telescope.nvim',
-    tag = 'v0.1.9',
-    dependencies = 'nvim-lua/plenary.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').load_extension('fzf')
+    end,
+  },
+
+  {
+    'stevearc/conform.nvim',
+    event = 'BufWritePre',
+    opts = {
+      formatters_by_ft = {
+        go = { 'goimports', 'gofumpt' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        yaml = { 'prettierd', 'prettier', stop_after_first = true },
+        lua = { 'stylua' },
+        terraform = { 'terraform_fmt' },
+        tf = { 'terraform_fmt' },
+        ['terraform-vars'] = { 'terraform_fmt' },
+      },
+      format_on_save = function(bufnr)
+        local ft = vim.bo[bufnr].filetype
+        local no_auto = { typescript = true, typescriptreact = true, javascript = true, javascriptreact = true }
+        if no_auto[ft] then return end
+        return { timeout_ms = 1000, lsp_format = 'fallback' }
+      end,
+    },
   },
 
   -- UI enhancements
-  'numToStr/Comment.nvim',
-  'lewis6991/gitsigns.nvim',
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      on_attach = function(bufnr)
+        local gs = require('gitsigns')
+        local b = vim.keymap.set
+        b('n', ']h', function() gs.nav_hunk('next') end, { buffer = bufnr, desc = 'Next hunk' })
+        b('n', '[h', function() gs.nav_hunk('prev') end, { buffer = bufnr, desc = 'Prev hunk' })
+        b('n', '<leader>hs', gs.stage_hunk, { buffer = bufnr, desc = 'Stage hunk' })
+        b('v', '<leader>hs', function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end,
+          { buffer = bufnr, desc = 'Stage selection' })
+        b('n', '<leader>hr', gs.reset_hunk, { buffer = bufnr, desc = 'Reset hunk' })
+        b('v', '<leader>hr', function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end,
+          { buffer = bufnr, desc = 'Reset selection' })
+        b('n', '<leader>hu', gs.undo_stage_hunk, { buffer = bufnr, desc = 'Undo stage hunk' })
+        b('n', '<leader>hp', gs.preview_hunk, { buffer = bufnr, desc = 'Preview hunk' })
+        b('n', '<leader>hb', function() gs.blame_line({ full = true }) end, { buffer = bufnr, desc = 'Blame line' })
+      end,
+    },
+  },
   'folke/which-key.nvim',
   {
     'nvim-lualine/lualine.nvim',
@@ -98,137 +177,76 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'folke/trouble.nvim',
+    opts = {},
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+    },
+  },
 })
 
 -- Keymaps
-local function keymap(mode, lhs, rhs, opts)
-  opts = opts or {}
-  vim.keymap.set(mode, lhs, rhs, opts)
-end
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Center on next match' })
+vim.keymap.set('n', 'N', 'Nzz', { desc = 'Center on prev match' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
+vim.keymap.set('i', 'fd', '<Esc>', { desc = 'Escape' })
 
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap('n', 'n', 'nzz', { desc = 'Center on next match' })
-keymap('n', 'N', 'Nzz', { desc = 'Center on prev match' })
-keymap('n', '<leader>j', ':setf json|%!jq<CR>', { desc = 'Format JSON' })
-keymap('i', 'fd', '<Esc>', { desc = 'Escape' })
-keymap('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Open file' })
-keymap('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Open buffer' })
-keymap('n', '<leader>/', function()
-  require('telescope.builtin').current_buffer_fuzzy_find()
-end, { desc = 'Search in buffer' })
-keymap('n', '<leader>k', require('telescope.builtin').live_grep, { desc = 'Grep workspace' })
+-- Telescope keymaps
+vim.keymap.set('n', '<leader>f', function() require('telescope.builtin').find_files() end, { desc = 'Open file' })
+vim.keymap.set('n', '<leader>b', function() require('telescope.builtin').buffers() end, { desc = 'Open buffer' })
+vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end,
+  { desc = 'Search in buffer' })
+vim.keymap.set('n', '<leader>k', function() require('telescope.builtin').live_grep() end, { desc = 'Grep workspace' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 
+-- LSP servers
 local servers = {
   clojure_lsp = {},
-  ts_ls = {},
-  gopls = {},
+  vtsls = {},
+  gopls = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+    },
+  },
   bashls = {},
   yamlls = {},
   jsonls = {},
   terraformls = {},
   kotlin_language_server = {},
-  lua_ls = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {
-          'vim',
-          'require'
-        },
-      },
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
+  zls = {},
+  lua_ls = {},
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-local on_attach = function(_, bufnr)
-  local b = vim.keymap.set
-  b('n', 'gd', require('telescope.builtin').lsp_definitions, { buffer = bufnr, desc = '[G]oto [D]efinition' })
-  b('n', 'gr', require('telescope.builtin').lsp_references, { buffer = bufnr, desc = '[G]oto [R]eferences' })
-  b('n', 'gi', require('telescope.builtin').lsp_implementations, { buffer = bufnr, desc = '[G]oto [I]mplementation' })
-  b('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover' })
-  b('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = '[R]e[n]ame' })
-  b('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = '[C]ode [A]ction' })
-  b('n', '<leader><leader>f', vim.lsp.buf.format, { buffer = bufnr, desc = '[F]ormat' })
-end
+local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 require('mason').setup()
 
 for server_name, config_opts in pairs(servers) do
   vim.lsp.config(server_name, {
     capabilities = capabilities,
-    on_attach = on_attach,
-    settings = config_opts ~= {} and config_opts or nil,
+    settings = next(config_opts) and config_opts or nil,
   })
 end
 
-require('mason-lspconfig').setup {
-  ensure_installed = vim.tbl_keys(servers),
-  automatic_enable = true,
-}
-
--- Completion
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
--- Treesitter
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    'clojure', 'typescript', 'go', 'bash', 'yaml', 'json',
-    'terraform', 'hcl', 'lua', 'vim', 'markdown', 'markdown_inline'
-  },
-  auto_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },
-  modules = {},
-  ignore_install = {},
-  sync_install = false,
-  parser_install_dir = nil,
-}
+vim.lsp.enable(vim.tbl_keys(servers))
 
 -- Diagnostics
 vim.diagnostic.config {
@@ -240,24 +258,44 @@ vim.diagnostic.config {
       [vim.diagnostic.severity.HINT] = '◆',
     },
   },
+  virtual_text = { spacing = 4, prefix = '●' },
   float = {
     border = 'rounded',
     style = 'minimal',
   },
 }
 
--- Diagnostic navigation with hover
-keymap('n', '[d', function()
-  vim.diagnostic.goto_prev()
-  vim.diagnostic.open_float(nil, { focusable = false, header = false })
-end, { desc = 'Previous diagnostic' })
+-- Autocommands
+local augroup = vim.api.nvim_create_augroup('UserConfig', { clear = true })
 
-keymap('n', ']d', function()
-  vim.diagnostic.goto_next()
-  vim.diagnostic.open_float(nil, { focusable = false, header = false })
-end, { desc = 'Next diagnostic' })
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = augroup,
+  callback = function(ev)
+    local bufnr = ev.buf
+    local tb = require('telescope.builtin')
+    vim.keymap.set('n', 'gd', tb.lsp_definitions, { buffer = bufnr, desc = 'Goto definition' })
+    vim.keymap.set('n', 'grr', tb.lsp_references, { buffer = bufnr, desc = 'Goto references' })
+    vim.keymap.set('n', 'gri', tb.lsp_implementations, { buffer = bufnr, desc = 'Goto implementation' })
+    vim.keymap.set('n', 'gy', tb.lsp_type_definitions, { buffer = bufnr, desc = 'Goto type definition' })
+    vim.keymap.set('n', '<leader>cf',
+      function() require('conform').format({ bufnr = bufnr, lsp_format = 'fallback' }) end,
+      { buffer = bufnr, desc = 'Format code' })
+  end,
+})
 
--- which-key setup with keybind mappings
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  group = augroup,
+  command = 'silent! normal! g`"zv',
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- which-key setup
 local wk = require('which-key')
 wk.setup {
   preset = 'helix',
@@ -265,60 +303,32 @@ wk.setup {
 }
 
 wk.add {
-  -- LSP mappings
   { '<leader>c',  group = 'LSP' },
-  { '<leader>ca', vim.lsp.buf.code_action,                  desc = 'Code action' },
-  { '<leader>cf', vim.lsp.buf.format,                       desc = 'Format code' },
-  { '<leader>cr', vim.lsp.buf.rename,                       desc = 'Rename symbol' },
-  { '<leader>ci', vim.lsp.buf.implementation,               desc = 'Goto implementation' },
-  { '<leader>ck', vim.lsp.buf.hover,                        desc = 'Hover' },
-  { '<leader>cd', vim.lsp.buf.definition,                   desc = 'Goto definition' },
-  { '<leader>cD', vim.lsp.buf.references,                   desc = 'Find references' },
 
-  -- Diagnostics
-  { '<leader>d',  group = 'Diagnostics' },
-  { '<leader>dd', vim.diagnostic.open_float,                desc = 'Show diagnostic' },
-  { '<leader>dq', vim.diagnostic.setloclist,                desc = 'Add diagnostics to loclist' },
-
-  -- File/Buffer navigation (already mapped but for display)
   { '<leader>f',  desc = 'Open file' },
   { '<leader>b',  desc = 'Open buffer' },
   { '<leader>/',  desc = 'Search in buffer' },
   { '<leader>k',  desc = 'Grep workspace' },
 
-  -- Telescope helpers
+  { '<leader>h',  group = 'Git hunks' },
   { '<leader>g',  group = 'Git/Grep' },
-  { '<leader>gf', require('telescope.builtin').git_files,   desc = 'Git files' },
+  { '<leader>gf', function() require('telescope.builtin').git_files() end,   desc = 'Git files' },
 
-  -- Search enhancements
   { '<leader>s',  group = 'Search' },
-  { '<leader>sd', require('telescope.builtin').diagnostics, desc = 'Diagnostics' },
-  { '<leader>sh', require('telescope.builtin').help_tags,   desc = 'Help' },
-  { '<leader>sr', require('telescope.builtin').resume,      desc = 'Resume last search' },
+  { '<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = 'Diagnostics' },
+  { '<leader>sh', function() require('telescope.builtin').help_tags() end,   desc = 'Help' },
+  { '<leader>sr', function() require('telescope.builtin').resume() end,      desc = 'Resume last search' },
 
-  -- Text manipulation
   { '<leader>t',  group = 'Text' },
-  { '<leader>tj', ':setf json|%!jq<CR>',                    desc = 'Format JSON' },
+  { '<leader>tj', function()
+    if vim.fn.executable('jq') == 0 then
+      vim.notify('jq is not installed', vim.log.levels.ERROR)
+      return
+    end
+    local ok, _ = pcall(vim.cmd, '%!jq')
+    if not ok then
+      vim.cmd('undo')
+      vim.notify('Buffer is not valid JSON', vim.log.levels.ERROR)
+    end
+  end, desc = 'Format JSON' },
 }
-
--- Copilot
-vim.g.copilot_filetypes = {
-  markdown = false,
-}
-
--- Autocommands
-local augroup = vim.api.nvim_create_augroup('UserConfig', { clear = true })
-
--- Restore cursor position
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  group = augroup,
-  command = 'silent! normal! g`"zv',
-})
-
--- Highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = augroup,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
